f8bc7cdbcfd557b66427e43b835449a6b5b96e3e
Andrew <asr322@drexel.edu>
2017-03-06 13:25:58 -0500

    RealToors/

---
+++ RealToors/scripts.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("TheSideBar").style.height = "215px";
+}           
+function closeNav() {
+    document.getElementById("TheSideBar").style.height = "0";
+}
